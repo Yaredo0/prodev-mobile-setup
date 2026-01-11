365233c450f31c845c9a08bb998ef134c3132210
Yaredo0 <yared4sy@gmail.com>
2026-01-12 01:32:18 +0300

    4

---
+++ prodev-mobile-app-0x03/styles/index.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+    position: "relative",
+  },
+  navGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    marginTop: 11,
+  },
+  largeText: {
+    fontSize: 40,
+    fontWeight: "700",
+  },
+  smallText: {
+    fontSize: 12,
+    color: "#7E7B7B",
+  },
+  // … continue typing the rest of the styles from your instructions …
+});
+
+export { styles };
